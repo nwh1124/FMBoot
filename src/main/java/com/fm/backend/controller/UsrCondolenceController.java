4800f95b03f46a90cb65311384d1647c7771c84c
nwh1124 <nwh0716@gmail.com>
2021-03-17 13:46:57 +0900

    조의문 리스팅 3개만

--- src/main/java/com/fm/backend/controller/UsrCondolenceController.java
+++ src/main/java/com/fm/backend/controller/UsrCondolenceController.java
@@ -1,5 +1,6 @@
 package com.fm.backend.controller;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +21,27 @@ public class UsrCondolenceController extends BaseController{
 
 	@GetMapping("/usr/condolence/list")
 	@ResponseBody
-	public ResultData shwoList(@RequestParam(defaultValue = "1") int boardId, String searchKeywordType, String searchKeyword, @RequestParam(defaultValue = "1") int page) {
+	public ResultData shwoList(@RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "1") int page) {
 		
 		List<Condolence> condolences = condolenceService.getForPrintCondolences(boardId);
 		
+		for(int i = condolences.size() - 4; i >= 0; i--) {
+			condolences.remove(i);
+		}
+		
 		for(Condolence condolence : condolences) {
-			String reduceRegDate = condolence.getRegDate().substring(0,10);
+			String reduceRegDate = condolence.getRegDate();
+			reduceRegDate = reduceRegDate.substring(2, 16).replace(" ", " | ");
 			
 			condolence.setRegDate(reduceRegDate);
+			
+			if(condolence.getBody().length() > 15) {
+				String reduceBody = condolence.getBody();
+				condolence.setBody(reduceBody.substring(0, 14) + "...");
+			}
 		}
+		
+		Collections.reverse(condolences);
 		
 		return new ResultData("S-1", "조의문 불러오기 완료", "condolences", condolences);
 	}
